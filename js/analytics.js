f42501d5063b34869a66eed1611266471ce181b3
Allen-Groads <68860305+Allen-Groads@users.noreply.github.com>
2020-11-17 18:04:09 +0700

    Add files via upload

---
+++ js/analytics.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+type: 'text/javascript',
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-133719811-2');
+
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym"); ym(68461966, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); 
